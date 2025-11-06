b1ebe9ca3b3a9cd0c2a9f33b4a9cbb29906b9b33
KendalSalazar <kendalsalazarumana@gmail.com>
2025-11-05 21:28:11 -0600

    Logica de publicidad y campos de publicidad en vistas

--- AutoClick/ViewComponents/CarruselPublicidadViewComponent.cs
+++ AutoClick/ViewComponents/CarruselPublicidadViewComponent.cs
@@ -19,9 +19,24 @@ namespace AutoClick.ViewComponents
             // formato: "cuadrado" o "horizontal"
             // seccion: nombre de la sección para filtrar anuncios
 
+            // Mapear formato a TamanoAnuncio
+            TamanoAnuncio tamanoFiltro;
+            if (formato == "horizontal")
+            {
+                tamanoFiltro = TamanoAnuncio.Horizontal;
+            }
+            else if (formato == "medio-vertical")
+            {
+                tamanoFiltro = TamanoAnuncio.MedioVertical;
+            }
+            else // "cuadrado" o cualquier otro valor usa GrandeVertical
+            {
+                tamanoFiltro = TamanoAnuncio.GrandeVertical;
+            }
+
             var anuncios = await _context.AnunciosPublicidad
                 .Include(a => a.EmpresaPublicidad)
-                .Where(a => a.Activo && a.EmpresaPublicidad != null)
+                .Where(a => a.Activo && a.EmpresaPublicidad != null && a.Tamano == tamanoFiltro)
                 .OrderByDescending(a => a.FechaPublicacion)
                 .Take(10) // Máximo 10 anuncios activos
                 .ToListAsync();
